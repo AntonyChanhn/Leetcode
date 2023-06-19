07f9937fb9ef111e4744a12d9c4c9608ef7fafaf
Antony chan <antony.hn.chan@gmail.com>
2023-06-19 23:41:12 +0800

    update leet704

---
+++ leet704.ts
@@ -0,0 +1,9 @@
+const search = (nums: number[], target: number): number => {
+  let output = -1;
+
+  return output;
+};
+console.log(search([-1, 0, 3, 5, 9, 12, 13, 14, 15, 16], 16)); //10
+console.log(search([-1, 0, 3, 5, 9, 12, 13, 14, 15, 16], 3)); //10
+console.log(search([-1, 0, 3, 5, 9, 12, 13, 14, 15], 14)); //9
+console.log(search([-1, 0, 3, 5, 9, 12, 13, 14, 15], 3)); //9
