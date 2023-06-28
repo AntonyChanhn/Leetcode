33309faad01bd72329ba6940d162843fe3facc69
Antony chan <antony.hn.chan@gmail.com>
2023-06-28 21:06:10 +0800

    update leetcode 707

--- problems/leet707.ts
+++ problems/leet707.ts
@@ -1,81 +1,87 @@
 class LinkedNode {
-    val:number;
-    next:LinkedNode | null;
+  val: number;
+  next: LinkedNode | null;
 
-    constructor(val?:number, next?:LinkedNode | null) {
-        this.val = (val === undefined ? 0 : val);
-        this.next = (next === undefined ? null : next);
-    }
+  constructor(val?: number, next?: LinkedNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
 }
 
 class MyLinkedList {
-    dummyHead:LinkedNode;
-    length:number;
-    constructor() {
-        this.dummyHead = new LinkedNode(0);
-        this.length = 0;
-    }
+  dummyHead: LinkedNode;
+  length: number;
+  constructor() {
+    this.dummyHead = new LinkedNode(0);
+    this.length = 0;
+  }
 
-    get(index: number): number {
-        if(index < 0 || index > this.length - 1) {
-            return -1
-        }
-        let cur = this.dummyHead.next;
-        while(index --) {
-            cur = cur!.next;
-        }
-        return cur!.val
+  get(index: number): number {
+    if (index < 0 || index > this.length - 1) {
+      return -1;
     }
-
-    addAtHead(val: number): void {
-        if(this.dummyHead.next){
-            this.dummyHead.next = new LinkedNode(val,this.dummyHead.next);
-        }else{
-            this.dummyHead.next = new LinkedNode(val);
-        }
-        this.length++
+    let cur = this.dummyHead.next;
+    while (index--) {
+      cur = cur!.next;
     }
+    return cur!.val;
+  }
 
-    addAtTail(val: number): void {
-        let cur = this.dummyHead.next;
-        let loop = this.length - 1;
-        while(loop --) {
-            cur = cur!.next;
-        }
-        cur!.next = new LinkedNode(val)
-        this.length++
+  addAtHead(val: number): void {
+    if (this.dummyHead.next) {
+      this.dummyHead.next = new LinkedNode(val, this.dummyHead.next);
+    } else {
+      this.dummyHead.next = new LinkedNode(val);
     }
+    this.length++;
+  }
 
-    addAtIndex(index: number, val: number): void {
-        if(index < 0 || index > this.length) {
-            throw new Error('exceed the length');
-        }
-        let cur: LinkedNode | null | undefined = this.dummyHead;
-        while( index --) {
-            cur = cur!.next;
-        }
-        cur!.next = new LinkedNode(val, cur!.next)
-        this.length++
+  addAtTail(val: number): void {
+    let cur: LinkedNode | null = this.dummyHead;
+    let loop = this.length;
+
+    while (loop--) {
+      cur = cur!.next;
     }
+    cur!.next = new LinkedNode(val);
+    this.length++;
+  }
 
-    deleteAtIndex(index: number): void {
-
+  addAtIndex(index: number, val: number): void {
+    if (index < 0 || index > this.length) {
+      return;
     }
+    let cur: LinkedNode | null = this.dummyHead;
+    while (index--) {
+      cur = cur!.next;
+    }
+    cur!.next = new LinkedNode(val, cur!.next);
+    this.length++;
+  }
+
+  deleteAtIndex(index: number): void {
+    if (index < 0 || index > this.length) {
+      return;
+    }
+    let cur: LinkedNode | null = this.dummyHead;
+    while (index--) {
+      cur = cur!.next;
+    }
+    cur!.next = cur!.next!.next;
+    this.length--;
+  }
 }
 
-
 //  Your MyLinkedList object will be instantiated and called as such:
-let obj = new MyLinkedList()
-obj.addAtHead(1);
-console.log("head: " + obj.get(0) );//MyLinkedList{val:1, next:null}
+let obj = new MyLinkedList();
+obj.addAtHead(2);
+console.log("head: " + obj.get(0));
 
-obj.addAtTail(3);
-console.log("tail: " + obj.get(0) + obj.get(1));//MyLinkedList{val:1, next:{val:3, next:null}}
+// obj.addAtTail(1);
+// console.log("tail: " + obj.get(0) + obj.get(1));
 
-obj.addAtIndex(2,2);
-console.log("index: " + obj.get(0) + obj.get(1) + obj.get(2));//MyLinkedList{val:1, next:{val:2, next:{val:3, next:null}}}
+// obj.addAtIndex(2, 2);
+// console.log("index: " + obj.get(0) + obj.get(1) + obj.get(2));
 
 obj.deleteAtIndex(1);
-console.log("delete: " + obj.get(0) + obj.get(1) + obj.get(2));//MyLinkedList{val:1, next:{val:3, next:null}}
-
- 
+console.log("delete: " + obj.get(0) + obj.get(1) + obj.get(2));
